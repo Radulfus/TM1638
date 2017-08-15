78ab6bd2d0d82efa72162c1f624e57c40f9f2ae1
Radulfus <cosmicso@gmx.net>
2017-08-15 10:52:56 +0200

    improved 16bit hex counter

--- tm1638cc.asm
+++ tm1638cc.asm
@@ -23,7 +23,6 @@
 
 .ORG $100
 
-
 ;=======================================================================
 ;	Initialisation
 ;	THIS IS REQUIRED - DONT CHANGE
@@ -165,8 +164,7 @@ dm_loop:
 
 
 b1:
-; button1 pressed
-
+; button1 pressed, back to the beginning "TM1638 DEMO"
 
 	rjmp	MAINLOOP
 
@@ -237,15 +235,23 @@ binloop:
 ;
 
 TM1638_COUNT_HEX:
-	push	AKKU
-	clr		AKKU
-hexloop:
-	rcall	TM1638_PRINT_HEX			; print value in AKKU
-	rcall	Delay100ms
-	inc		AKKU
-	cpi		AKKU, 0x00
-	brne	hexloop
-	pop		AKKU
+	push	AKKU2
+	push	AKKU3
+	ldi		XL,HIGH(0)
+	ldi		XH,LOW(0)
+z1loop:
+	mov		AKKU2, XL
+	mov		AKKU3, XH
+	rcall	TM1638_PRINT_HEX			; print values in AKKU2 and AKKU3
+										; as 16 bit number (LO and HI-Byte)
+	rcall 	Delay10ms
+	cpi 	XH, 11
+	breq	stop1_counting
+	adiw 	XH:XL,1	
+	brne 	z1loop
+stop1_counting:
+	pop		AKKU3
+	pop		AKKU2
 	ret
 
 ;
